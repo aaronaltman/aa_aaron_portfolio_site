459b7e7ca9ad7d4241bd4dd98acd333797f2254b
aka <aaronkaltman@gmail.com>
2023-12-18 09:37:43 -0500

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
