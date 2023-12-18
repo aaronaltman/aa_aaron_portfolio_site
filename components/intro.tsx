5d9ec756a19e6d5bba84915f823bb6c6b5b331ef
aka <aaronkaltman@gmail.com>
2023-12-18 09:55:38 -0500

    adding intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <section>
       <div className="flex justify-center items-center">
-        <div>
+        <div className="relative">
           <Image
             alt="Aaron K. Altman Bio"
             src="https://wp.rapidinsights.ai/wp-content/uploads/2023/10/how-to-market-new-legal-firm.png"
@@ -13,7 +13,9 @@ export default function Intro() {
             height={150}
             quality={90}
             priority={true}
+            className="rounded-full h-24 w-24 object-cover border-[0.35rem] border-white shadow-xl"
           />
+          <span className="text-4xl absolute bottom-0 right-0">👋</span>
         </div>
       </div>
     </section>
