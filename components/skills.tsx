81f0e7679e4dab92bce1a05258a553e7da3d7460
aka <aaronkaltman@gmail.com>
2023-12-18 19:31:33 -0500

    skills and experience timeline

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,26 +1,48 @@
 "use client";
 
 import React from "react";
+import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { motion } from "framer-motion";
-import SectionHeading from "./section-heading";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function Skills() {
   return (
-    <section className="mb-28 max-w-[53rem] scroll-mt-[28rem] text-center sm:mb-40">
-      <motion.div id="skills" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <SectionHeading>My skills</SectionHeading>
-        <ul className="flex flex-wrap justify-center items-center gap-2 text-lg text-gray-800">
-          {skillsData.map((skill, index) => (
-            <li
-              className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
-              key={skill}
-            >
-              {skill}
-            </li>
-          ))}
-        </ul>
-      </motion.div>
+    <section
+      id="skills"
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
     </section>
   );
 }
