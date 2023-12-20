c324a005c986f48bdc900afd40d863502c48bdbd
aka <aaronkaltman@gmail.com>
2023-12-19 19:26:13 -0500

    added Footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col mt-auto text-center p-4">
+      <small>&copy; Aaron K. Altman 2024 and beyond</small>
+      <small>Powered by React, Node.js, Next.js, Framer Motion, Vercel</small>
+    </footer>
+  );
+}
