2b6558d027d14d7f600cf2ab640811c484e0a54c
aka <aaronkaltman@gmail.com>
2023-12-20 05:08:39 -0500

    this should work

--- components/about.tsx
+++ components/about.tsx
@@ -34,9 +34,9 @@ export default function About() {
 
       <p>
         <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className="font-medium">learning new things</span>. I am
-        currently learning about{" "}
+        playing video games, watching movies, and playing with my dog (pooch). I
+        also enjoy <span className="font-medium">learning new things</span>. I
+        am currently learning about{" "}
         <span className="font-medium">history and philosophy</span>. I&apos;m
         also learning how to play the guitar.
       </p>
